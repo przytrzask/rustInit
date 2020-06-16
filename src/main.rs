1d644fec010045e901bbb000bf69b319a1d26b3d
Tomasz Trzaskoma <tomasztrzaskoma@Tomaszs-MacBook-Pro.local>
2020-06-16 14:21:38 +0200

    rust functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 fn main() {
+    // variables
 let mut name = "Pascal";
-
     println!("{}",name);
-
     name = "Alice";
+    println!("{} and {}", name, name);
 
-    println!("{}",name
-);
+    // functions
+    let first = "Tomek".to_string();
+    let last = "Trzask".to_string();
+    say_name(first,last);
 
+}
+
+fn say_name(first: String, last: String) {
+
+    println!("{} {}", first,last);
 }
